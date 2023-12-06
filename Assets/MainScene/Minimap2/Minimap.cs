038934ec05a35780ae0df59ee3f521a4bf793952
100451218 <98971345+100451218@users.noreply.github.com>
2023-12-06 01:42:07 +0000

    minimap save

---
+++ Assets/MainScene/Minimap2/Minimap.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;  // Reference to the player's Transform
+    public Image allyPointPrefab;
+    public Image enemyPointPrefab;
+
+    private void Start()
+    {
+        // Create points for allies and enemies (adjust positions accordingly)
+        CreateAllyPoint(new Vector3(10f, 0f, 10f));
+        CreateEnemyPoint(new Vector3(-5f, 0f, -5f));
+    }
+
+    private void Update()
+    {
+        // Optionally, update minimap orientation based on player's rotation
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+
+    private void CreateAllyPoint(Vector3 position)
+    {
+        Image allyPoint = Instantiate(allyPointPrefab, transform);
+        SetPointPosition(allyPoint, position);
+    }
+
+    private void CreateEnemyPoint(Vector3 position)
+    {
+        Image enemyPoint = Instantiate(enemyPointPrefab, transform);
+        SetPointPosition(enemyPoint, position);
+    }
+
+    private void SetPointPosition(Image point, Vector3 worldPosition)
+    {
+        // Convert world position to minimap position
+        Vector3 localPosition = new Vector3(worldPosition.x, worldPosition.z, 0f);
+        point.rectTransform.anchoredPosition = localPosition;
+    }
+}
--- Assets/MainScene/Notas.cs
+++ Assets/MainScene/Notas.cs
@@ -2,6 +2,7 @@
 ahora mismo el soldier no escucha el primer comando (o tarda unos cuantos segundazos en hacer caso);
 
 
+Opcion 1 del minimapa, es una imagen y unos sprites se mueven escalados y acorde, faltaria posicionar el objeto arriba a la derecha y tirando, no me gusta mucho por ahora...
 
 
 
