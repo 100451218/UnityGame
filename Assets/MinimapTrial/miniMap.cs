15fd2efaef8ce273e204301a899b3804af9cd0b7
100451218 <98971345+100451218@users.noreply.github.com>
2023-12-07 20:50:58 +0000

    minimap start

---
+++ Assets/MinimapTrial/miniMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class miniMap : MonoBehaviour {
+	public Vector3 fixedPosition = new Vector3(-1200, 200f,-48.7f);
+    public Vector3 fixedRotation = new Vector3(0f, 0f, 0f); // Adjust as needed
+
+    void LateUpdate()
+    {
+		Debug.Log(fixedPosition);
+        // Keep the camera's position fixed
+        gameObject.transform.position = fixedPosition;
+		Debug.Log(gameObject.transform.position+ ""+ gameObject.name);
+        // Set the camera's rotation to a fixed value
+        gameObject.transform.rotation = Quaternion.Euler(fixedRotation);
+    }
+	void Update(){
+		Debug.Log(gameObject.transform.position+ ""+ gameObject.name);
+	}
+}
